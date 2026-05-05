8445f92785f74d3e9e2bb7b0a181a70ed04b56b0
yankiweiss <yakovw2706@gmail.com>
2026-05-05 12:06:21 -0400

    update

--- api/index.js
+++ api/index.js
@@ -1,29 +1,24 @@
 import express from 'express';
+import serverless from 'serverless-http';
 import dotenv from 'dotenv';
 
-import cors from 'cors'
+import cors from 'cors';
 import router from './routes/api/property.js';
 import eventRouter from './routes/api/event.js';
 import investorRouter from './routes/api/investor.js';
 import investmentRouter from './routes/api/investments.js';
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
-
 app.use(express.json());
 
+app.use('/api/properties', router);
+app.use('/api/event', eventRouter);
+app.use('/api/investor', investorRouter);
+app.use('/api/investment', investmentRouter);
 
-app.use('/api/properties', router)
-app.use('/api/event', eventRouter)
-app.use('/api/investor', investorRouter )
-app.use('/api/investment', investmentRouter )
-
-app.listen(PORT, ()=> {
-    console.log(`APP ruining on ${PORT}`)
-})
+export default serverless(app);
 
 
