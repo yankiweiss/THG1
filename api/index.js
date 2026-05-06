3876e12486568bc91c146243d6edbc8a088cf140
yankiweiss <yakovw2706@gmail.com>
2026-05-06 15:07:06 -0400

    update

--- api/index.js
+++ api/index.js
@@ -12,7 +12,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 
 //app.use('/api/property', propertyRouter);
 app.use('/api/event', eventRouter);
