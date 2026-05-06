568e99d8363dae002617bf6fca283f882bb3f7b4
yankiweiss <yakovw2706@gmail.com>
2026-05-06 16:50:25 -0400

    update

--- api/index.js
+++ api/index.js
@@ -7,9 +7,9 @@ import cors from 'cors';
 const app = express();
 
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "1mb" }));
+
+
 
 
 //import propertyRouter from './routes/property.js';
