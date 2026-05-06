c54e09f47629d2a0ffa01129a2ad45be7bb22618
yankiweiss <yakovw2706@gmail.com>
2026-05-06 16:02:20 -0400

    update

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use('/api/investor', investorRouter);
 //app.use('/api/investments', investmentsRouter);
 
-console.log(investmentsRouter)
+
 
 
 
