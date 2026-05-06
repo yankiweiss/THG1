8ca79bfe789eeb3173d361176b2838fc78ab3b26
yankiweiss <yakovw2706@gmail.com>
2026-05-06 13:01:41 -0400

    update

--- api/controllers/propertyController.js
+++ api/controllers/propertyController.js
@@ -1,5 +1,5 @@
-import dataBasePool from "../model/db.js";
-
+//import dataBasePool from "../model/db.js";
+//
 //const postAProperty = async (req, res) => {
 //  const { property_name, purchase_price, investors, pictures, closing_date } =
 //    req.body;
@@ -68,146 +68,146 @@ import dataBasePool from "../model/db.js";
 //    client.release();
 //  }
 //};
-
-
-const getAllProperties = async (req, res) => {
-  const getAllPropertiesDB = `
-    SELECT * FROM properties`;
-
-    console.log('query started')
-
-    console.log(process.env.DATABASE_URL);
-
-    let result;
-
-  // will need to get for each property the investor associate data.
-
-  try {
-    result = await dataBasePool.query(getAllPropertiesDB);
-  } catch (error) {
-    console.error(error);
-  return res.status(500).json({ error: "Server error" });
-    
-  }
-
-
-console.log("QUERY DONE");
-
-  return res.json(result.rows);
-};
-
-
-
-
-const getPropertyById = async (req, res) => {
-  const propertyId = parseInt(req.params.id, 10);
-
-  const client = await dataBasePool.connect();
-
-  try {
-    const propertyResult = await client.query(
-      `SELECT *
-        FROM properties WHERE id = $1`,
-      [propertyId]
-    );
-
-
-
-
-    const investorResult = await client.query(
-      `
-      SELECT 
-        i.id AS investor_id,
-        i.name AS investor_name,
-        inv.invested_amount,
-        inv.perf_return,
-        inv.role,
-        inv.id AS investment_id
-      FROM investors i
-      JOIN investments inv ON inv.investor_id = i.id
-      WHERE inv.property_id = $1
-      `,
-      [propertyId]
-    );
-
-    if (propertyResult.rows.length === 0) {
-  return res.status(404).json({ error: "Property not found" });
-}
-
-
-    res.json({
-      ...propertyResult.rows[0],
-      investors: investorResult.rows.map(row => ({
-        id: row.investor_id,
-        name: row.investor_name,
-        invested_amount: row.invested_amount,
-        perf_return: row.perf_return,
-        role: row.role,
-        investment_id: row.investment_id
-      })),
-
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error", error });
-  } finally {
-    client.release();
-  }
-};
-
-
-const deleteProperty = async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const result = await dataBasePool.query(
-      `DELETE FROM properties WHERE id = $1 RETURNING *`,
-      [id],
-    );
-
-    res.json({
-      message: "Property deleted successfully",
-      data: result.rows,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
-  }
-};
-
-const updatePropertyField = async (req, res) => {
-  const { id } = req.params;
-  const { field, value } = req.body;
-
-  try {
-
-    const result = await dataBasePool.query(
-      `UPDATE properties
-      SET ${field} = $1
-      WHERE id = $2
-      RETURNING *; `,
-      [value, id]
-
-    )
-
-    res.json(result.rows[0]);
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Update failed" });
-  }
-
-
-}
-
-
-
-
-
-export {
-  //postAProperty,
-  getAllProperties,
-  getPropertyById,
-  deleteProperty,
-  updatePropertyField
-};
+//
+//
+//const getAllProperties = async (req, res) => {
+//  const getAllPropertiesDB = `
+//    SELECT * FROM properties`;
+//
+//    console.log('query started')
+//
+//    console.log(process.env.DATABASE_URL);
+//
+//    let result;
+//
+//  // will need to get for each property the investor associate data.
+//
+//  try {
+//    result = await dataBasePool.query(getAllPropertiesDB);
+//  } catch (error) {
+//    console.error(error);
+//  return res.status(500).json({ error: "Server error" });
+//    
+//  }
+//
+//
+//console.log("QUERY DONE");
+//
+//  return res.json(result.rows);
+//};
+//
+//
+//
+//
+//const getPropertyById = async (req, res) => {
+//  const propertyId = parseInt(req.params.id, 10);
+//
+//  const client = await dataBasePool.connect();
+//
+//  try {
+//    const propertyResult = await client.query(
+//      `SELECT *
+//        FROM properties WHERE id = $1`,
+//      [propertyId]
+//    );
+//
+//
+//
+//
+//    const investorResult = await client.query(
+//      `
+//      SELECT 
+//        i.id AS investor_id,
+//        i.name AS investor_name,
+//        inv.invested_amount,
+//        inv.perf_return,
+//        inv.role,
+//        inv.id AS investment_id
+//      FROM investors i
+//      JOIN investments inv ON inv.investor_id = i.id
+//      WHERE inv.property_id = $1
+//      `,
+//      [propertyId]
+//    );
+//
+//    if (propertyResult.rows.length === 0) {
+//  return res.status(404).json({ error: "Property not found" });
+//}
+//
+//
+//    res.json({
+//      ...propertyResult.rows[0],
+//      investors: investorResult.rows.map(row => ({
+//        id: row.investor_id,
+//        name: row.investor_name,
+//        invested_amount: row.invested_amount,
+//        perf_return: row.perf_return,
+//        role: row.role,
+//        investment_id: row.investment_id
+//      })),
+//
+//    });
+//  } catch (error) {
+//    console.error(error);
+//    res.status(500).json({ message: "Server error", error });
+//  } finally {
+//    client.release();
+//  }
+//};
+//
+//
+//const deleteProperty = async (req, res) => {
+//  try {
+//    const { id } = req.params;
+//
+//    const result = await dataBasePool.query(
+//      `DELETE FROM properties WHERE id = $1 RETURNING *`,
+//      [id],
+//    );
+//
+//    res.json({
+//      message: "Property deleted successfully",
+//      data: result.rows,
+//    });
+//  } catch (err) {
+//    console.error(err);
+//    res.status(500).json({ error: "Server error" });
+//  }
+//};
+//
+//const updatePropertyField = async (req, res) => {
+//  const { id } = req.params;
+//  const { field, value } = req.body;
+//
+//  try {
+//
+//    const result = await dataBasePool.query(
+//      `UPDATE properties
+//      SET ${field} = $1
+//      WHERE id = $2
+//      RETURNING *; `,
+//      [value, id]
+//
+//    )
+//
+//    res.json(result.rows[0]);
+//
+//  } catch (err) {
+//    console.error(err);
+//    res.status(500).json({ error: "Update failed" });
+//  }
+//
+//
+//}
+//
+//
+//
+//
+//
+//export {
+//  //postAProperty,
+//  getAllProperties,
+//  getPropertyById,
+//  deleteProperty,
+//  updatePropertyField
+//};
