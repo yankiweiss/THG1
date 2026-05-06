9883611151ac173fd8003d606306dd271735d0fd
yankiweiss <yakovw2706@gmail.com>
2026-05-06 16:42:02 -0400

    update

--- api/index.js
+++ api/index.js
@@ -2,15 +2,16 @@ import express from 'express';
 import serverless from 'serverless-http';
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from 'cors';
 
 const app = express();
 
 
 app.use(cors());
-app.use(express.json({ limit: "100mb" }));
-//app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-import cors from 'cors';
+
 //import propertyRouter from './routes/property.js';
 //import eventRouter from './routes/event.js';
 import investorRouter from './routes/investor.js';
