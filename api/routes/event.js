7e7d8a7d04e5176c265719c5fa82c20734274c77
yankiweiss <yakovw2706@gmail.com>
2026-05-06 15:26:49 -0400

    update

--- api/routes/event.js
+++ api/routes/event.js
@@ -1,12 +1,12 @@
-//import express from 'express';
-//import postAEvent from '../controllers/eventController.js';
-//const eventRouter = express.Router();
-//
-//
-//eventRouter.post('/', postAEvent)
-//
-//
-//
-//
-//
-//export default eventRouter;
+import express from 'express';
+import postAEvent from '../controllers/eventController.js';
+const eventRouter = express.Router();
+
+
+eventRouter.post('/', postAEvent)
+
+
+
+
+
+export default eventRouter;
