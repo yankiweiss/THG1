31c8598f236e686f1c19259b10f0c8facb4bd1c5
yankiweiss <yakovw2706@gmail.com>
2026-05-06 22:08:46 -0400

    update

--- api/index.js
+++ api/index.js
@@ -19,8 +19,8 @@ app.use("/api/event", eventRouter);
 app.use("api/investor", investorRouter);
 app.use("/api/investments", investmentsRouter);
 
-//const PORT =3000;
-//
-//app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
+const PORT =3000;
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
 
 export default app;
