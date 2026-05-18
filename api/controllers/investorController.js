091e9954ae962235c79aa658abdb711a685c84e2
yankiweiss <yakovw2706@gmail.com>
2026-05-18 17:23:34 -0400

    update

--- api/controllers/investorController.js
+++ api/controllers/investorController.js
@@ -37,7 +37,7 @@ const getInvestorByID = async (req, res) => {
   p.property_name AS property_name,
   p.secure_url AS image_url,
   p.closing_date AS closing_date,
-  json_agg(json_build_object('event_date', e.event_date, 'event_amount', e.event_amount, 'event_type', e.event_type, 'from',e.from_date, 'to', e.to_date)) 
+  json_agg(json_build_object('event_date', e.event_date, 'event_amount', e.event_amount, 'event_type', e.event_type, 'from',e.from, 'to', e.to)) 
   FILTER (WHERE e.id IS NOT NULL) AS events
   FROM investments inv 
   INNER JOIN investors i 
